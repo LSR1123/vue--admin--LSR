5f9ad8fdbc20296e6aefe41e325b226382ceb43c
LSR1123 <939268457@qq.com>
2020-01-03 15:39:18 +0800

    我提交了顾客管理

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '家政预约服务公司',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | true
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
